d1640b8f70fbe180952ed69f593005333b104402
dahub <dev.david.andre@gmail.com>
2016-07-12 16:30:07 +0200

    commit avant merge

---
+++ DaChess.Ui/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(DaChess.Ui.Startup))]
+namespace DaChess.Ui
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
